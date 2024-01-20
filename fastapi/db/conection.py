723974051bcb386b1ed7f71db8542c2a4178de7c
KamiALK <69237651+KamiALK@users.noreply.github.com>
2024-01-20 17:32:02 -0500

    /

--- fastapi/db/conection.py
+++ fastapi/db/conection.py
@@ -8,7 +8,7 @@ base de datos : JUGADORES
 TABLA: usuarios
 
 """
-from sqlalchemy import create_engine, MetaData
+
 
 engine = create_engine("mysql+pymysql://root:123456@localhost:3306/JUGADORES")
 Meta_Data = MetaData()

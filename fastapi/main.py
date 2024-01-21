f021546f395a09027b66d11cc051062819c8cb47
KamiALK <69237651+KamiALK@users.noreply.github.com>
2024-01-20 22:59:41 -0500

    .'

--- fastapi/main.py
+++ fastapi/main.py
@@ -3,9 +3,13 @@ from  sqlalchemy.orm import Session
 from fastapi.responses import HTMLResponse
 from typing import Annotated
 from router.routers import app
-
-
+import router.crud
+from db.conection import engine,Session,User,Base
 from schema.user_schema import User,Userschemanoid,Usernopass
+
+
+
+
 
 
 

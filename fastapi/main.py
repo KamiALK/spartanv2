7bfe37f0d3abd49ade87da4882bd1f8190646397
kamilo <kamilo@localhost.localdomain>
2024-02-10 14:28:20 -0500

    .

--- fastapi/main.py
+++ fastapi/main.py
@@ -60,18 +60,18 @@ async def root():
 
 
 @appi.get("/api/users/", response_model=list[UserID])
-def get_users (db:Session=Depends(get_db)):    
+def get_users (db=Depends(get_db)):    
     return router.crud.get_users(db=db)
 
 @appi.get("/api/user/{id:int}",response_model=UserID)
-def get_user(id, db:Session=Depends(get_db)):
+def get_user(id, db=Depends(get_db)):
     u = router.crud.get_user_by_id(db=db,id=id)
     if u is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="no se ha encontrado el usuario")
     return router.crud.get_user_by_id(db=db, id=id)
 
 @appi.post("/api/create",response_model=UserData)
-async def create_user(user:UserData,db:Session=Depends(get_db)):
+async def create_user(user:UserData,db=Depends(get_db)):
     created_user = router.crud.create_user(db=db, user=user)
     if created_user is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario ya existe")
@@ -84,13 +84,13 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
 @appi.post("/token")
-async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: Session = Depends(get_db) ):
+async def login_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db = Depends(get_db) ):
     return login_for_access_token( form_data=form_data,db=db)   
 
 
 
 @appi.get("/users/me/", response_model=Usernopass)
-async def read_users_me(db:Session=Depends(get_db),
+async def read_users_me(db=Depends(get_db),
     current_user:Usernopass=Depends(router.crud.get_current_user)
 ):
     username=current_user
@@ -99,7 +99,7 @@ async def read_users_me(db:Session=Depends(get_db),
 
 
 @appi.get("/users/me/items/")
-async def read_own_items(db:Session=Depends(get_db),
+async def read_own_items(db=Depends(get_db),
     current_user:Usernopass=Depends(router.crud.get_current_user)
 ):
     username=current_user

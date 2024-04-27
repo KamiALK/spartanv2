8403ee006c4ffaffd63df31c00c2a4ff45e6f2de
kmialk <andresyalca@gmail.com>
2024-04-26 19:06:29 -0500

    .

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,28 +1,21 @@
-from fastapi import FastAPI, Query, Request, Form,Depends,HTTPException, status
-from  sqlalchemy.orm import Session
-import router.crud 
+from fastapi import FastAPI, Query, Request, Form, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+import router.crud
 from fastapi.responses import HTMLResponse
 from db.conection import Session
 from schema.user_schema import UserData
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from router.crud import login_for_access_token
-from fastapi import Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
-from fastapi import Request
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
 from fastapi import Path
-from fastapi import Query
 import router.router_estructura as partido_router
-import os
 
 
 # aqui la configuracion de archivos jin2
-appi =FastAPI()
-
+appi = FastAPI()
 
 
 appi.mount("/static", StaticFiles(directory="static"), name="static")
@@ -30,10 +23,8 @@ appi.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-
 # Aca estamos configurando los cors
 origin = [
-
     "http://localhost:4321",
     "http://localhost",
     "http://localhost:3000",
@@ -46,11 +37,10 @@ appi.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    
 )
 
 
-#aqui la configuracion de la base de datos
+# aqui la configuracion de la base de datos
 def get_db():
     db = Session()
     try:
@@ -61,11 +51,13 @@ def get_db():
 
 appi.include_router(partido_router.router)
 
-'''
+"""
 uvicorn main:appi --reload 
 
 
-'''
+"""
+
+
 @appi.get("/", response_class=HTMLResponse)
 async def root(request: Request, tipo: str = Query(None)):
     if tipo in ["jugador", "arbitro", "evaluador", "normal"]:
@@ -76,60 +68,94 @@ async def root(request: Request, tipo: str = Query(None)):
         return templates.TemplateResponse("dash.html", {"request": request})
 
 
-
 @appi.get("/{tipo}/", response_class=HTMLResponse)
-async def get_users_all(request: Request,tipo: str = Path(...),db=Depends(get_db)):
-    usuarios = router.crud.get_users(db=db,tipo=tipo)
+async def get_users_all(request: Request, tipo: str = Path(...), db=Depends(get_db)):
+    usuarios = router.crud.get_users(db=db, tipo=tipo)
     print("Valor de tipo antes de llamar a get_user_by_email:", tipo)
-    return templates.TemplateResponse("index.html", {"request": request, "tipo": tipo,  "usuarios": usuarios})
-
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "tipo": tipo, "usuarios": usuarios}
+    )
 
 
 @appi.get("/{tipo}/{id:int}", response_class=HTMLResponse)  # Cambia ID a id
-def get_user(request: Request, id, db=Depends(get_db), tipo: str = Path(...)):  # Cambia ID a id
+async def get_user(
+    request: Request, id, db=Depends(get_db), tipo: str = Path(...)
+):  # Cambia ID a id
     usuarios = router.crud.get_user_by_id(db=db, tipo=tipo, ID=id)  # Cambia ID a id
-    
+
     if usuarios is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="no se ha encontrado el usuario")
+            detail="no se ha encontrado el usuario",
+        )
     return templates.TemplateResponse(
-        "lista_filter_id.html", 
-        {"request": request,  "usuarios": usuarios})
+        "lista_filter_id.html", {"request": request, "usuarios": usuarios}
+    )
 
 
 @appi.get("/{tipo}/cedula/{cedula:int}", response_class=HTMLResponse)  # Cambia ID a id
-def get_user(request: Request, cedula, db=Depends(get_db), tipo: str = Path(...)):  # Cambia a cedula
-    usuarios = router.crud.get_user_by_cedula(db=db, tipo=tipo, cedula=cedula)  # Cambia ID a id
-    
+async def get_user(
+    request: Request, cedula, db=Depends(get_db), tipo: str = Path(...)
+):  # Cambia a cedula
+    usuarios = router.crud.get_user_by_cedula(
+        db=db, tipo=tipo, cedula=cedula
+    )  # Cambia ID a id
+
     if usuarios is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail="no se ha encontrado el usuario")
+            detail="no se ha encontrado el usuario",
+        )
     return templates.TemplateResponse(
-        "lista_filter_cedula.html", 
-        {"request": request,  "usuarios": usuarios})
+        "lista_filter_cedula.html", {"request": request, "usuarios": usuarios}
+    )
+
 
 #!!!!!!!!!!!!!!!!!!!!!!!     creacion de usuario
 @appi.post("/usuarios/registro", response_model=UserData)
-async def create_user(request: Request, username: str = Form(...), nombre: str = Form(...), apellido: str = Form(...), celular: int = Form(...), edad: int = Form(...), cedula: int = Form(...), genero: str = Form(...), email: str = Form(...), passwd: str = Form(...), tipo: str = Form(...), db=Depends(get_db)):
-    user_data = UserData(username=username, nombre=nombre, apellido=apellido, celular=celular, edad=edad, cedula=cedula, genero=genero, email=email, passwd=passwd)
+async def create_user(
+    request: Request,
+    username: str = Form(...),
+    nombre: str = Form(...),
+    apellido: str = Form(...),
+    celular: int = Form(...),
+    edad: int = Form(...),
+    cedula: int = Form(...),
+    genero: str = Form(...),
+    email: str = Form(...),
+    passwd: str = Form(...),
+    tipo: str = Form(...),
+    db=Depends(get_db),
+):
+    user_data = UserData(
+        username=username,
+        nombre=nombre,
+        apellido=apellido,
+        celular=celular,
+        edad=edad,
+        cedula=cedula,
+        genero=genero,
+        email=email,
+        passwd=passwd,
+    )
     new_user = router.crud.create_new_user(db=db, user=user_data, tipo=tipo)
     if new_user is None:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario ya existe")
-    
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario ya existe"
+        )
+
     return new_user
 
-@appi.get("/usuarios/registro", response_class=HTMLResponse)
 
-async def create_user_get(request: Request, db=Depends(get_db),):
+@appi.get("/usuarios/registro", response_class=HTMLResponse)
+async def create_user_get(
+    request: Request,
+    db=Depends(get_db),
+):
     return templates.TemplateResponse("registro.html", {"request": request})
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
-
 
 
 #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!     login de usuario
@@ -139,47 +165,55 @@ async def obtener_tipo(request: Request, tipo: str):
     print(tipo)
     return {"tipo": tipo}
 
+
 @appi.post("/token")
 async def login_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(),
     tipo: str = Form(...),  # Agrega el parámetro tipo
-    db = Depends(get_db)
+    db=Depends(get_db),
 ):
     # print("Valor de tipo antes de llamar a get_user_by_email:", tipo)
-    token_data = login_for_access_token(form_data=form_data, db=db, tipo=tipo)  # Pasa el tipo al método de autenticación
-    token_type = token_data['token_type']
+    token_data = login_for_access_token(
+        form_data=form_data, db=db, tipo=tipo
+    )  # Pasa el tipo al método de autenticación
+    token_type = token_data["token_type"]
     if not token_data:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    
-    response = RedirectResponse(url="/users/me", status_code=302)
-    response.set_cookie(key="token", value=token_data['access_token'], domain="localhost", path="/")
-    response.set_cookie(key="token_type", value=token_type, domain="localhost", path="/")
 
-    #estoy intentanto inyectar el id para poder maniobrar en toda la pagina
+    response = RedirectResponse(url="/users/me", status_code=302)
+    response.set_cookie(
+        key="token", value=token_data["access_token"], domain="localhost", path="/"
+    )
+    response.set_cookie(
+        key="token_type", value=token_type, domain="localhost", path="/"
+    )
+
+    # estoy intentanto inyectar el id para poder maniobrar en toda la pagina
     # response.set_cookie(key="ID", value=str(token_data['ID']), domain="localhost", path="/")
     return response
 
 
-
 @appi.get("/users/me/", response_class=HTMLResponse)
-async def read_users_me(request: Request, db=Depends(get_db),
-                        current_user: dict = Depends(router.crud.get_current_user)):
+async def read_users_me(
+    request: Request,
+    db=Depends(get_db),
+    current_user: dict = Depends(router.crud.get_current_user),
+):
     try:
         if current_user["tipo"] == "admin":
-            return templates.TemplateResponse("admin.html", {"request": request, "current_user": current_user})
+            return templates.TemplateResponse(
+                "admin.html", {"request": request, "current_user": current_user}
+            )
         elif current_user["tipo"] == "Arbitros":
-            return templates.TemplateResponse("user.html", {"request": request, "current_user": current_user})
+            return templates.TemplateResponse(
+                "user.html", {"request": request, "current_user": current_user}
+            )
         elif current_user["tipo"] == "Evaluadores":
-            return templates.TemplateResponse("user_dos.html", {"request": request, "current_user": current_user})
+            return templates.TemplateResponse(
+                "user_dos.html", {"request": request, "current_user": current_user}
+            )
         else:
             raise HTTPException(status_code=401, detail="Unauthorized")
-        
-    
+
     except Exception as e:
         raise HTTPException(status_code=500, detail="Internal server error")
-
-
-
-
-
-
